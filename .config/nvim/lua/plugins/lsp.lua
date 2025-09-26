7ed56532aa681a9db69205d9260a673aaa80c911
Arthur Hassan Souki <souki.arthur@gmail.com>
2025-09-26 11:11:40 -0300

    Update lsp.lua
    ajustando para novo meto de inicializar servidores lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -10,7 +10,6 @@ return {
                 pylsp = {},
                 cssls = {},
                 angularls = require('config.lsp.angularls'),
-                -- angularls = {},
                 ts_ls = {},
                 zls = {},
                 csharp_ls = {},
@@ -18,11 +17,12 @@ return {
             },
         },
         config = function(_, opts)
-            local lspconfig = require("lspconfig")
             for server, config in pairs(opts.servers) do
                 config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-                lspconfig[server].setup(config)
+                vim.lsp.config(server, config)
+                vim.lsp.enable(server)
             end
+
         end,
     },
 }
